80149c8a229cfc27a3b00ea640b9b4bc4af652bb
Bhanu <65509340+KBhanup@users.noreply.github.com>
2022-07-18 17:43:00 -0400

    Update mag_test.py

--- nmpc/scripts/mag_test.py
+++ nmpc/scripts/mag_test.py
@@ -5,5 +5,8 @@ from Mag_Eng_DisEng_fns import Mag
 
 Sensor_MagON = Mag(4)
 Sensor_MagOFF =Mag(15)
+Drone_Mag = Mag(4)
 
 Sensor_MagON.dr_Magdisengage()
+#Sensor_MagOFF.Sn_Magdisengage()
+#Drone_Mag.dr_Magdisengage()
